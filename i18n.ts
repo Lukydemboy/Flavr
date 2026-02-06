0a3b24a8fd8d73c3b54959c9cb117d4ef4c96360
Lukas Demeyere <lukasdemeyere@hotmail.com>
2026-02-06 16:29:51 +0100

    Remove language detector which is only available in browser

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,4 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import { Language } from './domain/enums/language.enum';
 import en from './translations/en.json';
@@ -10,19 +9,16 @@ import de from './translations/de.json';
 import * as SecureStore from 'expo-secure-store';
 import { StorageKeys } from './hooks/storage';
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: Language.English,
-    resources: {
-      [Language.English]: { translation: en },
-      [Language.French]: { translation: fr },
-      [Language.Dutch]: { translation: nl },
-      [Language.Spanish]: { translation: es },
-      [Language.German]: { translation: de },
-    },
-  });
+i18n.use(initReactI18next).init({
+  fallbackLng: Language.English,
+  resources: {
+    [Language.English]: { translation: en },
+    [Language.French]: { translation: fr },
+    [Language.Dutch]: { translation: nl },
+    [Language.Spanish]: { translation: es },
+    [Language.German]: { translation: de },
+  },
+});
 
 export const changeLanguage = async (language: Language): Promise<void> => {
   await i18n.changeLanguage(language);
